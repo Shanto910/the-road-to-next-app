22efcaadf3316acbf018c9be4f3935d8969c2f3e
Shanto910 <shantoopaul@gmail.com>
2025-03-15 17:13:12 +0600

    add: custom heading component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,12 @@
 import Link from 'next/link';
+import Heading from '@/components/Heading';
 import { ticketsPath } from '@/paths';
 
 const HomePage = () => {
 	return (
 		<div className="flex-1 flex flex-col gap-y-8">
-			<div>
-				<h2 className="text-3xl font-bold tracking-tight">Home Page</h2>
-				<p className="text-sm text-muted-foreground">This is the home page</p>
-			</div>
+			<Heading title="Home Page" description="This is the home page" />
+
 			<div className="flex-1 flex flex-col items-center">
 				<Link href={ticketsPath()} className="underline">
 					Go to Tickets

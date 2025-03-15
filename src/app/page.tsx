aac7ca3fedae82bc115e913faed850560504175f
Shanto910 <shantoopaul@gmail.com>
2025-03-15 13:49:17 +0600

    used clsx to apply conditional css classes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,16 @@ import { ticketsPath } from '@/paths';
 
 const HomePage = () => {
 	return (
-		<div>
-			<h1 className="text-lg">Home Page</h1>
-			<Link href={ticketsPath()} className="underline">
-				Tickets
-			</Link>
+		<div className="flex-1 flex flex-col gap-y-8">
+			<div>
+				<h2 className="text-3xl font-bold tracking-tight">Home Page</h2>
+				<p className="text-sm text-muted-foreground">This is the home page</p>
+			</div>
+			<div className="flex-1 flex flex-col items-center">
+				<Link href={ticketsPath()} className="underline">
+					Go to Tickets
+				</Link>
+			</div>
 		</div>
 	);
 };
